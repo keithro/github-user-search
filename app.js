05140a2d9debbfb8f98c17ab1a11b42cd8c7dc70
Keith Rodriguez <rodriguez.keith@gmail.com>
2018-02-09 01:14:33 -0500

    Add profile info and fetch repos

--- app.js
+++ app.js
@@ -17,13 +17,15 @@ searchUser.addEventListener('keyup', (e) => {
       .then(data => {
         if(data.profile.message === 'Not Found') {
           // Show Alert
-
+          ui.showAlert('User not found', 'alert alert-danger');
         } else {
           // Show profile
           ui.showProfile(data.profile);
+          ui.showRepos(data.repos);
         }
       })
   } else {
     // Clear profile
+    ui.clearProfile();
   }
 })
--- github.js
+++ github.js
@@ -2,15 +2,20 @@ class GitHub {
   constructor() {
     this.client_id = '9b4d07bd172820e72562';
     this.client_secret = '23f1cfec06a3b4e8b3d6eb93a3c521021fba9c68';
+    this.repos_count = 5;
+    this.repos_sort = 'created: asc';
   }
 
   async getUser(user) {
     const profileResponse = await fetch(`https://api.github.com/users/${user}?client_id=${this.client_id}&client_secret=${this.client_secret}`);
+    const repoResponse = await fetch(`https://api.github.com/users/${user}/repos?per_page=${this.repos_count}&sort=${this.repos_sort}&client_id=${this.client_id}&client_secret=${this.client_secret}`);
 
     const profile = await profileResponse.json();
+    const repos = await repoResponse.json();
 
     return {
-      profile // profile: profile
+      profile, // profile: profile
+      repos // repos: repos
     }
   }
 }
